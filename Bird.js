50d848149d28b298fe59b247798dbc436ca753b6
Murat Güven <murat.guven98@hotmail.com>
2018-03-03 16:18:54 +0300

    Update Bird.js

--- Bird.js
+++ Bird.js
@@ -55,7 +55,7 @@ class Bird {
   show() {
     push();
     translate(this.pos.x, this.pos.y);
-    image(img,0,0,img.width*a/img.height,a);
+    image(img,0,0,img.width*2*a/img.height,2*a);
     pop();
   }
 }
